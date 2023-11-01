52951419b3774dd603a943ea4a4d3e8faa01e03c
Mosquera-lag <mosqueras688@gmail.com>
2023-11-01 21:00:17 +0000

    hola

--- src/paginas/Registro.jsx
+++ src/paginas/Registro.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Menu from "../componentes/Menu";
 import Singin from "../componentes/singin";
 import Piedepagina from '../componentes/Piedepagina';
+import FormularioRegistro from '../componentes/FormularioRegistro';
 
 function Registro(){
   return (
     <div className="Registro">
       <Menu/>
-      <Singin/>
+      <FormularioRegistro/>
       <Piedepagina/>
     </div>
   );
